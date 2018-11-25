a25363f6bcfb175e8823d5ba0cfaf90f368afe86
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2018-11-26 01:11:15 +0900

    feat: :tada: fontawesome追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import DefaultLayout from "@/layouts/Default.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faTimes);
 
 import "@/styles/core/reset.scss";
 import "@/styles/core/base.scss";
 
 export default function(Vue) {
   Vue.component("Layout", DefaultLayout);
+  Vue.component("font-awesome-icon", FontAwesomeIcon);
 }
