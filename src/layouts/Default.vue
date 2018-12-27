42ec7f3bede37fd95d4e738c65b2b1d623bad7eb
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2018-12-27 23:58:51 +0900

    refactor: :+1: フッターを常に画面下に設置

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -36,14 +36,20 @@ export default {
 <style lang="scss" scoped>
 @import "@/styles/utility/transition-page.scss";
 .Default {
+  display: grid;
+  grid-template-rows: 1fr 50px;
+  grid-template-columns: auto;
+  min-height: 100vh;
+
   &_Contents {
     max-width: 1500px;
-    padding: 24px 36px;
-    margin: 32px auto;
+    padding: 56px 36px;
+    margin: 0 auto;
 
     @include mq-xs {
+      max-width: 100vw;
       padding: 24px 15px;
-      margin: 88px auto;
+      margin: 88px auto 24px;
     }
   }
 }
