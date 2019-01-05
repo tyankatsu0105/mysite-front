ea4a370b53eab9dfee13f21463b3279bf10bdc72
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-01-06 02:08:58 +0900

    feat: :tada: tag managerの組み込み

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import VuePrism from "vue-prism";
 import store from "@/store";
+import TagManager from "@/thirdparty/TagManager";
 
 import "@/styles/core/fonts.scss";
 import "@/styles/core/reset.scss";
 import "@/styles/core/base.scss";
 
 import "prismjs/themes/prism-tomorrow.css";
+
+TagManager();
 
 export default function(Vue, { head }) {
   head.meta.push({
