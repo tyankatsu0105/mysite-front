d4bd4f120ed0b85279f7b51a14b6aa72c3e99023
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-02-09 23:55:48 +0900

    perf: :tada: Search console

--- src/main.js
+++ src/main.js
@@ -11,10 +11,16 @@ import "prismjs/themes/prism-tomorrow.css";
 TagManager();
 
 export default function(Vue, { head }) {
-  head.meta.push({
-    name: "viewport",
-    content: "width=device-width, initial-scale=1"
-  });
+  head.meta.push([
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1"
+    },
+    {
+      name: "google-site-verification",
+      content: "DcneOuaFfensAf9vHJG_AE21c0W8CKhZIqKlYwxnzvo"
+    }
+  ]);
   Vue.use(VuePrism);
   Vue.use(store);
 }
