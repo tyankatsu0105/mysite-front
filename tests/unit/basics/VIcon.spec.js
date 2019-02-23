dc7462d31db97d36af4415f2b590544ad7f66c52
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-02-23 14:15:22 +0900

    test: :green_heart: Update test

--- tests/unit/basics/VIcon.spec.js
+++ tests/unit/basics/VIcon.spec.js
@@ -1,25 +1,14 @@
 import { shallowMount } from "@vue/test-utils";
 import VIcon from "@/basics/VIcon.vue";
 
-const propsData = {
-  name: "hatena"
-};
-
 describe("VIcon", () => {
   it("render", () => {
     const wrapper = shallowMount(VIcon, {
-      propsData
+      slots: {
+        default: '<svg class="icon"></svg>'
+      }
     });
 
     expect(wrapper.html()).toMatchSnapshot();
-  });
-
-  describe("computed", () => {
-    it("icon", () => {
-      const wrapper = shallowMount(VIcon, {
-        propsData
-      });
-      expect(wrapper.vm.icon).toEqual("icon-hatena");
-    });
   });
 });
