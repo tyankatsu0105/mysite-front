5310f2733a0a03c39c959f513ac4541b9022a368
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-02-23 11:56:25 +0900

    add index

---
+++ src/pages/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>Hi. I'm tyankatsu. Thanks to visit my website.</p>
+    <p>
+      Please check
+      <g-link href="https://tyankatsu.com"> https://tyankatsu.com </g-link>
+    </p>
+  </div>
+</template>
