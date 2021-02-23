db0257a8b9b02ed4c4c01b737be0d6f3f058c0c9
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-02-23 11:39:27 +0900

    add text

--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
-import store from "@/store";
-import TagManager from "@/thirdparty/TagManager";
-
 import "@/styles/core/fonts.scss";
 import "@/styles/core/reset.scss";
 import "@/styles/core/base.scss";
 
-import "prismjs/themes/prism-tomorrow.css";
-
-if (process.browser) {
-  TagManager();
-}
-
-export default function(Vue) {
-  Vue.use(store);
-}
+export default function () {}
